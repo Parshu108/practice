d01237e5a2e745fc9fc666e7f711f2773ca870f7
Adi-D-king <as4497119@gmail.com>
2024-01-20 19:26:57 +0530

    hhy

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,10 +1,21 @@
-// Your First C++ Program
+// // Your First C++ Program
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter a number:";
+//     cin >> n ;
+//     cout << "The squre of "<< n <<" is " <<n*n;
+//     return 0;
+// }
+//----20-01-2023
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    cout << "Enter a number:";
-    cin >> n ;
-    cout << "The squre of "<< n <<" is " <<n*n;
+    int a=90;
+    cout << "address oa a =";
+    cout << &a;
+    cout << "\nvalue of a = ";
+    cout << a;
     return 0;
 }
