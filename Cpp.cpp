29ea7178cb3cbb2233d06ae449137d377a93bcae
Adi-D-king <as4497119@gmail.com>
2024-01-20 18:56:39 +0530

    yh

---
+++ Cpp.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number:";
+    cin >> n ;
+    cout << "The squre of "<< n <<" is " <<n*n;
+    return 0;
+}
